161cbd52a3fae273f607533b0eccb48e7682d4b1
jiazhipeng <jzpyoung@gmail.com>
2016-11-02 15:24:33 +0800

    DateCons

---
+++ src/main/java/org/jzp/code/common/component/constant/DateConstants.java
@@ -0,0 +1,51 @@
+package org.jzp.code.common.component.constant;
+
+/**
+ * 时间格式常量类
+ *
+ * @author jiazhipeng
+ * @version 1.0
+ * @date 2016-11-02
+ */
+public class DateConstants {
+
+    /**
+     * 年-月-日
+     **/
+    public static final String YYYY_MM_DD = "yyyy-MM-dd";
+
+    /**
+     * 年月日
+     **/
+    public static final String YYYYMMDD = "yyyyMMdd";
+
+    /**
+     * 时:分
+     **/
+    public static final String HH_MM = "HH:mm";
+
+    /**
+     * 分:秒
+     **/
+    public static final String MM_SS = "mm:ss";
+
+    /**
+     * 时:分:秒
+     **/
+    public static final String HH_MM_SS = "HH:mm:ss";
+
+    /**
+     * 年月日时分秒
+     **/
+    public static final String YYYYMMDDHHMMSS = "yyyyMMddHHmmss";
+
+    /**
+     * 年-月-日 24时:分:秒
+     **/
+    public static final String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
+
+    /**
+     * 年-月-日 24时:分:秒.毫秒
+     **/
+    public static final String YYYY_MM_DD_HH_MM_SS_SSS = "yyyy-MM-dd HH:mm:ss.SSS";
+}
